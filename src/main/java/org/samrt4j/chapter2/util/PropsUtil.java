8021a00f9fbbf9c5812ea4ac2ceb3a1a1db4569e
coco <1174450457@qq.com>
2017-09-16 17:14:32 +0800

    增加两个工具类方法

--- src/main/java/org/samrt4j/chapter2/util/PropsUtil.java
+++ src/main/java/org/samrt4j/chapter2/util/PropsUtil.java
@@ -3,6 +3,8 @@ package org.samrt4j.chapter2.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Properties;
+
 
 /**
  * 属性文件工具类
@@ -11,6 +13,99 @@ public final class PropsUtil {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PropsUtil.class);
 
+    /**
+     * 加载属性文件
+     *
+     * @param fileName
+     * @return
+     */
+    public static Properties loadProps(String fileName) {
+        Properties props = null;
+        //TODO
+        return null;
+    }
+
+    /**
+     * 获取字符串属性 （默认为空字符串）
+     *
+     * @param props
+     * @param key
+     * @return
+     */
+    public static String getString(Properties props, String key) {
+        return getString(props, key, "");
+    }
+
+    /**
+     * 获取字符串属性（可指定默认值）
+     *
+     * @param props
+     * @param key
+     * @param defaultValue
+     * @return
+     */
+    private static String getString(Properties props, String key, String defaultValue) {
+        String value = defaultValue;
+        if (props.containsKey(key)) {
+            value = props.getProperty(key);
+        }
+        return value;
+    }
+
+
+    /**
+     * 获取数值型属性 （默认为 0）
+     *
+     * @param props
+     * @param key
+     * @return
+     */
+    public static int getInt(Properties props, String key) {
+        return getInt(props, key, 0);
+    }
+
+    /**
+     * 获取数值型属性（可指定默认值）
+     *
+     * @param props
+     * @param key
+     * @param defaultValue
+     * @return
+     */
+    private static int getInt(Properties props, String key, int defaultValue) {
+        int value = defaultValue;
+        if (props.containsKey(key)) {
+            value = CastUtil.castInt(props.getProperty(key));
+        }
+        return value;
+    }
+
+    /**
+     * 获取布尔型属性 （默认为 false）
+     *
+     * @param props
+     * @param key
+     * @return
+     */
+    public static boolean getBoolean(Properties props, String key) {
+        return getBoolean(props, key, false);
+    }
+
+    /**
+     * 获取布尔型属性（可指定默认值）
+     *
+     * @param props
+     * @param key
+     * @param defaultValue
+     * @return
+     */
+    private static boolean getBoolean(Properties props, String key, boolean defaultValue) {
+        boolean value = defaultValue;
+        if (props.containsKey(key)) {
+            value = CastUtil.castBoolean(props.getProperty(key));
+        }
+        return value;
+    }
 
 
 }
